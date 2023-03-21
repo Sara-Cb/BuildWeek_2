edbeb1d184a676e11fa2970ab1751130b6dc261c
Antonio Cancemi <antoniocancemi1@gmail.com>
2023-03-21 17:51:16 +0100

    player funziona ma non scorre

--- assets/js/albums.js
+++ assets/js/albums.js
@@ -1,7 +1,7 @@
 const API_URL = "https://striveschool-api.herokuapp.com/api/deezer/";
 let thisAlbum = [];
 let albumId = new URLSearchParams(window.location.search).get("id");
-
+let song;
 const getData = async function (_content, _array) {
   try {
     let response = await fetch(API_URL + _content);
@@ -41,16 +41,20 @@ let printAlbum = async function () {
 
     </div>`;
     for (let j = 0; j < thisAlbum[i].tracks.data.length; j++) {
+      let preview = '"' + thisAlbum[i].tracks.data[j].preview + '"';
+      //let id = "track" + j;
+      console.log(preview);
+
       albumRef.innerHTML += ` <div class="row">
       
-      <div class="col"><a class="d-flex" href="albums.html?id=${thisAlbum[i].id}&track=${
+      <div  onclick="playFunction(\'${
         thisAlbum[i].tracks.data[j].preview
-      }">
+      }\')" class="col d-flex">
       <span class="align-self-center mx-3 fs-3">${j + 1}</span>
       <span class="d-flex flex-column justify-content-around">
       <h4 class="my-0 ">${thisAlbum[i].tracks.data[j].title}</h4>
       <p class="fs-5 my-0 ">${thisAlbum[i].artist.name}<p></span>
-    </a></div>
+    </div>
     <div class="col-2 d-flex">
     <h4 class="align-self-center">${thisAlbum[i].tracks.data[j].rank}</h4>
   </div>
@@ -58,7 +62,60 @@ let printAlbum = async function () {
   <h4 class="align-self-center">${thisAlbum[i].tracks.data[j].duration}</h4>
 </div>
     </div>`;
+      // let divRef = document.getElementById(id);
+      //console.log(divRef);
+      //divRef.addEventListener("click", function () {
+      //playFunction(preview);
+      //console.log("click rilevato");
+      //});
     }
   }
 };
+let audio;
+const playBtn = document.getElementById("playbutton");
+const progressEl = document.querySelector('input[type="range"]');
+const playFunction = function (_song) {
+  //console.log(_song);
+  if (audio) {
+    audio.pause();
+  }
+  audio = new Audio(_song);
+  audio.paused ? audio.play() : audio.pause();
+  playBtn.textContent = audio.paused ? "▶️" : "⏸️";
+  playBtn.click();
+  console.log("click su play");
+};
+
+let mouseDownOnSlider = false;
+if (audio) {
+  audio.addEventListener("loadeddata", () => {
+    progressEl.value = 0;
+  });
+  audio.addEventListener("timeupdate", () => {
+    if (!mouseDownOnSlider) {
+      progressEl.value = (audio.currentTime / audio.duration) * 100;
+    }
+  });
+  audio.addEventListener("ended", () => {
+    playBtn.textContent = "▶️";
+  });
+
+  playBtn.addEventListener("click", () => {
+    console.log("ciao");
+    audio.paused ? audio.play() : audio.pause();
+    playBtn.textContent = audio.paused ? "▶️" : "⏸️";
+  });
+
+  progressEl.addEventListener("change", () => {
+    const pct = progressEl.value / 100;
+    audio.currentTime = (audio.duration || 0) * pct;
+  });
+  progressEl.addEventListener("mousedown", () => {
+    mouseDownOnSlider = true;
+  });
+  progressEl.addEventListener("mouseup", () => {
+    mouseDownOnSlider = false;
+  });
+}
+
 printAlbum();
