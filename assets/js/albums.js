dee3fe622b5e0b1dd77497af764dbf6e1cbe897b
Sara-Cb <saramaiden23@gmail.com>
2023-03-22 14:21:14 +0100

    fetch everything e print everything

--- assets/js/albums.js
+++ assets/js/albums.js
@@ -2,6 +2,74 @@ const API_URL = "https://striveschool-api.herokuapp.com/api/deezer/";
 let thisAlbum = [];
 let albumId = new URLSearchParams(window.location.search).get("id");
 let song;
+const tracklistRef = document.getElementById("tracklist");
+const playerSong = document.getElementById("playerSong");
+const playerArtist = document.getElementById("playerArtist");
+const songTime = document.getElementById("songTime");
+const timeRange = document.getElementById("timeRange");
+const playerImg = document.getElementById("playerImg");
+const playButton = document.getElementById("playbutton");
+const volumeControl = document.getElementById("volume");
+let audio = document.getElementById("audioReference");
+
+timeRange.addEventListener("click", function (event) {
+  const pos = (event.pageX - timeRange.offsetLeft) / timeRange.offsetWidth;
+  audio.currentTime = pos * audio.duration;
+});
+
+playButton.addEventListener("click", function () {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
+
+volumeControl.addEventListener("input", function () {
+  audio.volume = volumeControl.value / 10;
+});
+
+function playFunction(url, title, artist, cover, duration) {
+  audio.src = url;
+
+  playerSong.textContent = title;
+  playerArtist.textContent = artist;
+  playerImg.setAttribute("src", cover);
+
+  audio.addEventListener("loadedmetadata", function () {
+    songTime.children[0].textContent = formatTime(0);
+    songTime.children[2].textContent = formatTime(Math.round(duration));
+
+    timeRange.style.width = "0%";
+
+    audio.play();
+  });
+
+  audio.addEventListener("timeupdate", function () {
+    songTime.children[0].textContent = formatTime(
+      Math.round(audio.currentTime)
+    );
+    timeRange.style.width = (audio.currentTime / audio.duration) * 100 + "%";
+  });
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+}
+
+const calcDuration = function (time) {
+  minuti = Math.round(time / 60);
+  secondi = time % 60;
+  if (secondi < 10) {
+    secondi = "0" + secondi;
+  }
+  let correctTime = `${minuti}:${secondi}`;
+  console.log(correctTime);
+  return correctTime;
+};
+
 const getData = async function (_content, _array) {
   try {
     let response = await fetch(API_URL + _content);
@@ -22,7 +90,8 @@ let printAlbum = async function () {
   console.log(thisAlbum);
   for (let i = 0; i < thisAlbum.length; i++) {
     let albumRef = document.querySelector("#album");
-    albumRef.innerHTML = ` <div class="col-3 border border-solid border-dark">
+    albumRef.innerHTML = ` 
+    <div class="col-3 border border-solid border-dark">
       <img
         class="image-fluid w-100"
         src="${thisAlbum[i].cover_big}"
@@ -41,40 +110,35 @@ let printAlbum = async function () {
 
     </div>`;
     for (let j = 0; j < thisAlbum[i].tracks.data.length; j++) {
-      let preview = '"' + thisAlbum[i].tracks.data[j].preview + '"';
+      let preview = thisAlbum[i].tracks.data[j].preview;
+      let songTitle = thisAlbum[i].tracks.data[j].title;
+      let songArtist = thisAlbum[i].artist.name;
+      let songCover = thisAlbum[i].cover_big;
+      let songTime = thisAlbum[i].tracks.data[j].duration;
+      let reproductions = thisAlbum[i].tracks.data[j].rank;
       //let id = "track" + j;
       console.log(preview);
 
-      albumRef.innerHTML += ` <div class="row">
-      
-      <div  onclick="playFunction(\'${
-        thisAlbum[i].tracks.data[j].preview
-      }\')" class="col d-flex">
-      <span class="align-self-center mx-3 fs-3">${j + 1}</span>
-      <span class="d-flex flex-column justify-content-around">
-      <h4 class="my-0 ">${thisAlbum[i].tracks.data[j].title}</h4>
-      <p class="fs-5 my-0 ">${thisAlbum[i].artist.name}<p></span>
-    </div>
-    <div class="col-2 d-flex">
-    <h4 class="align-self-center">${thisAlbum[i].tracks.data[j].rank}</h4>
-  </div>
-  <div class="col-2 d-flex">
-  <h4 class="align-self-center">${thisAlbum[i].tracks.data[j].duration}</h4>
-</div>
-    </div>`;
-      // let divRef = document.getElementById(id);
-      //console.log(divRef);
-      //divRef.addEventListener("click", function () {
-      //playFunction(preview);
-      //console.log("click rilevato");
-      //});
+      tracklistRef.innerHTML += `<li><div class="row">
+      <div onclick="playFunction(\'${preview}\', \'${songTitle}\', \'${songArtist}\',\'${songCover}\', \'${songTime}\')" class="col d-flex">
+        <span class="align-self-center mx-3 fs-3">${j + 1}</span>
+        <span class="d-flex flex-column justify-content-around">
+          <h4 class="my-0 ">${songTitle}</h4>
+          <p class="fs-5 my-0 ">${songArtist}<p>
+        </span>
+      </div>
+      <div class="col-2 d-flex">
+        <h4 class="align-self-center">${reproductions}</h4>
+      </div>
+      <div class="col-2 d-flex">
+        <h4 class="align-self-center">${calcDuration(songTime)}</h4>
+      </div>
+      </div></li>`;
     }
   }
 };
-let audio;
-const playBtn = document.getElementById("playbutton");
-const progressEl = document.querySelector('input[type="range"]');
-const playFunction = function (_song) {
+
+/*const playFunction = function (_song) {
   //console.log(_song);
   if (audio) {
     audio.pause();
@@ -116,6 +180,6 @@ if (audio) {
   progressEl.addEventListener("mouseup", () => {
     mouseDownOnSlider = false;
   });
-}
+}*/
 
 printAlbum();
--- assets/js/modules/mainFunctions.js
+++ assets/js/modules/mainFunctions.js
@@ -1,43 +1,39 @@
 const tracksArray = [];
-const albumsArray = [];
-const artistsArray = [];
+const stampaPlace = document.getElementById("stampaqui");
 
-const fetchAlbum = function (albumId) {
-  async () => {
-    try {
-      const response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      let data = await response.json();
-      tracklist = albumData.tracks.data;
+const printEverything = function () {
+  const tracksDiv = document.createElement("div");
+  tracksDiv.setAttribute("class", "tracks");
+  const albumsDiv = document.createElement("div");
+  albumsDiv.setAttribute("class", "albums");
+  const artistsDiv = document.createElement("div");
+  artistsDiv.setAttribute("class", "artists");
+  for (let i = 0; i < tracksArray.length; i++) {
+    let track = tracksArray[i];
+    let album = track.album;
+    let artist = track.artist;
 
-      let album = data;
-      albumsArray.push(album);
-    } catch (error) {
-      console.error(`Error: ${error.message}`);
-    }
-  };
-};
+    const trackCard = document.createElement("div");
+    trackCard.setAttribute("class", "card");
+    const albumCard = document.createElement("div");
+    albumCard.setAttribute("class", "card");
+    const artistCard = document.createElement("div");
+    artistCard.setAttribute("class", "card");
 
-const fetchArtist = function (artistId) {
-  async () => {
-    try {
-      const response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/deezer/artist/${artistId}`
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      let data = await response.json();
-      let artist = data.data;
-      artistsArray.push(artist);
-    } catch (error) {
-      console.error(`Error: ${error.message}`);
-    }
-  };
+    trackCard.innerHTML = `<div class='row'><div class="col-3 border border-solid border-dark card-img"><img class="image-fluid" src="${album.cover_big}" style='width:100px; aspect-ratio:1/1' alt="Album cover"/></div><div class="col-7 border border-solid border-dark"><div class='card-body'><h2>${track.title}</h2><p>${artist.name}</p></div></div><div class="col-2 border border-solid border-dark"><p>${track.duration}</p></div></div>`;
+
+    albumCard.innerHTML = `<div class="border border-solid border-dark card-img-top"> <img class="image-fluid" src="${album.cover_big}" style='width:100px; aspect-ratio:1/1' alt="Album cover"/> </div><div class="col-7 border border-solid border-dark"> <div class='card-body'> <h2>${album.title}</h2> <p>${artist.name}</p> </div> </div>`;
+
+    artistCard.innerHTML = `<div class="border border-solid border-dark card-img-top"> <img class="image-fluid" src="${artist.picture_big}" style='width:100px; aspect-ratio:1/1' alt="Album cover"/> </div> <div class="col-7 border border-solid border-dark"> <div class='card-body'><h2>${artist.name}</h2><p>Artista</p> </div></div>`;
+
+    tracksDiv.appendChild(trackCard);
+    albumsDiv.appendChild(albumCard);
+    artistsDiv.appendChild(artistCard);
+  }
+
+  stampaPlace.appendChild(tracksDiv);
+  stampaPlace.appendChild(albumsDiv);
+  stampaPlace.appendChild(artistsDiv);
 };
 
 const fetchEverything = async (keyword) => {
@@ -53,17 +49,12 @@ const fetchEverything = async (keyword) => {
     for (let i = 0; i < arrayTracce.length; i++) {
       let track = data.data[i];
       tracksArray.push(track);
-      let album = track.album;
-      albumsArray.push(album);
-      let artist = track.artist;
-      artistsArray.push(artist);
     }
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
   console.log(tracksArray);
-  console.log(albumsArray);
-  console.log(artistsArray);
+  printEverything();
 };
 
 fetchEverything("america");
@@ -74,12 +65,14 @@ const playerSong = document.getElementById("playerSong");
 const playerArtist = document.getElementById("playerArtist");
 const songTime = document.getElementById("songTime");
 const timeRange = document.getElementById("timeRange");
+const playerImg = document.getElementById("playerImg");
 
 function playFunction(url, title, artist, cover, duration) {
   const audio = new Audio(url);
 
   playerSong.textContent = title;
   playerArtist.textContent = artist;
+  playerImg.setAttribute("src", cover);
 
   audio.addEventListener("loadedmetadata", function () {
     songTime.children[0].textContent = formatTime(0);
